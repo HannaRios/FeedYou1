e75c4674554c93589740e88762f93e0a805c5032
HannaRios <hannariosrios2@gmail.com>
2026-03-27 19:40:25 -0500

    Fix: Express 5 wildcard syntax

--- server/index.js
+++ server/index.js
@@ -85,7 +85,7 @@ export { io };
 // ================== FRONTEND ==================
 app.use(express.static(path.join(__dirname, "..", "dist")));
 
-app.get("*", (req, res) => {
+app.get("/:path*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
 });
 
